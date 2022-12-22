5a85ea936880bbd94c9849582e63c4a4f8f20706
Abilov Jeyhun <abilovv599@gmail.com>
2022-12-22 06:42:45 +0400

    junk files deleted

--- practice-vite/vite-first/src/components/inputComp/index.jsx
+++ practice-vite/vite-first/src/components/inputComp/index.jsx
@@ -9,6 +9,26 @@ const Table = () => {
   //     };
   const [myArr, setMyArr] = useState(arr);
 
+  /////Edit Status/////
+
+  const [formStatus, setFormStatus] = useState(false);
+
+  // const [nameStatus, setNameStatus] = useState("");
+  // const [emailStatus, setEmailStatus] = useState("");
+
+  const [editBtnId, setEditBtnId] = useState("");
+
+  const [inputNameValue, setInputNameValue] = useState("");
+  const [inputEmailValue, setInputEmailValue] = useState("");
+
+  const handleEdit = (e) => {
+    setFormStatus(true);
+    // setNameStatus();
+    setEditBtnId(e.target.parentNode.parentNode.children[0].innerText);
+    setInputNameValue(e.target.parentNode.parentNode.children[1].innerText);
+    setInputEmailValue(e.target.parentNode.parentNode.children[2].innerText);
+  };
+
   //   const handleDelete = (e) => {
   //     console.log(e.target.current);
   //     // let deleteTr = myArr.filter(
@@ -18,7 +38,7 @@ const Table = () => {
   //   };
 
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       {/* <input ref={inputElement} placeholder="type here ..." type="text" />
       <button onClick={focusInput}>Click me</button> */}
       <table>
@@ -29,6 +49,7 @@ const Table = () => {
             <th>Email</th>
             <th>Edit</th>
             <th>Delete</th>
+            <th>Add to Local Storage</th>
           </tr>
         </thead>
         <tbody>
@@ -39,7 +60,7 @@ const Table = () => {
                 <td>{elem.name}</td>
                 <td>{elem.email}</td>
                 <td>
-                  <button>Edit</button>
+                  <button onClick={(e) => handleEdit(e)}>Edit</button>
                 </td>
                 <td>
                   <button
@@ -55,11 +76,46 @@ const Table = () => {
                     Delete
                   </button>
                 </td>
+                <td>
+                  <button>Add</button>
+                </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      {formStatus && (
+        <div style={{ marginLeft: "20px" }}>
+          <form
+            style={{ display: "flex", flexDirection: "column" }}
+            action=""
+            onSubmit={(e) => {
+              e.preventDefault();
+              setFormStatus(false);
+              myArr.find((el) => el.id == editBtnId).name = inputNameValue;
+              myArr.find((el) => el.id == editBtnId).email = inputEmailValue;
+              setMyArr([...myArr]);
+              setInputNameValue("");
+              setInputEmailValue("");
+            }}
+          >
+            <label htmlFor="">Name:</label>
+            <input
+              value={inputNameValue}
+              onChange={(e) => setInputNameValue(e.target.value)}
+              type="text"
+            />
+            <label htmlFor="">Email:</label>
+            <input
+              value={inputEmailValue}
+              onChange={(e) => setInputEmailValue(e.target.value)}
+              type="text"
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
